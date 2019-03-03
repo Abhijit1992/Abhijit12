ef56d70244469b5abb17ffae8d2f1b9897b4df9a
Abhijit1992 <bubai.mullick@gmail.com>
2019-03-03 17:36:07 +0530

    Python

--- Demo.py
+++ Demo.py
@@ -1,4 +1,19 @@
 from selenium import webdriver
-qspiders = webdriver.Chrome(executable_path="C:\Driver\chromedriver.exe")
-#qspiders = webdriver.Firefox(executable_path="C:\Driver\geckodriver.exe")
-qspiders.get("https://www.google.com")
+import time
+driver = webdriver.Chrome(executable_path="C:/Driver/chromedriver.exe")
+driver.get("https://phptravels.com/")
+driver.maximize_window()
+driver.implicitly_wait(30)
+win_id= driver.current_window_handle
+print(win_id)
+time.sleep(15)
+driver.find_element_by_xpath("//span[text()='Login']").click()
+
+mul_win_id=driver.window_handles
+print(mul_win_id)
+print(type(mul_win_id))
+print(mul_win_id[1])
+driver.switch_to.window(mul_win_id[1])
+driver.find_element_by_id("inputEmail").send_keys("testt")
+driver.switch_to.window(win_id)
+driver.close()
